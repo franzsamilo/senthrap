654ec6b37c2b11449a455a6a9145f0267b712ecb
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-01-31 18:55:18 +0800

    [Feature] Complete Auth + Screen Screen Navs

---
+++ senthrap/pages/screens/Chat/Chat.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+function Chat() {
+  return (
+    <div>
+      <h1>Chat</h1>
+      <h2>Yo chat is this real?</h2>
+    </div>
+  )
+}
+
+export default Chat
--- senthrap/pages/screens/Home/Home.tsx
+++ senthrap/pages/screens/Home/Home.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from "react"
+import { useUser } from "@auth0/nextjs-auth0/client"
+import { useRouter } from "next/router"
+import Image from "next/image"
 
 function Home() {
+  const { user } = useUser()
+  const router = useRouter()
+
+  function auth0Logout() {
+    router.push("/api/auth/logout")
+  }
+
+  function goToChat() {
+    router.push("/screens/Chat/Chat")
+  }
+
   return (
-    <div>
-      hello dad I am home
+    <div className="flex flex-col">
+      <div className="flex flex-row mt-4">
+        <Image
+          src={user?.picture || ""}
+          alt={user?.name || ""}
+          width={64}
+          height={64}
+          className="rounded-full"
+        />
+        <h1 className="text-xl ml-4 mt-4">{user?.name}</h1>
+      </div>
+      <p className="m-4">{user?.email}</p>
+      <button
+        className="px-10 py-2 rounded-full font-semibold hover:text-main"
+        onClick={goToChat}
+      >
+        Go to chat
+      </button>
+      <button
+        className="px-10 py-2 rounded-full font-semibold hover:text-main"
+        onClick={auth0Logout}
+      >
+        Logout
+      </button>
     </div>
   )
 }
