0f4b07ff8e2ed6ae5a25788cdf84109690125204
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-02-01 16:07:48 +0800

    [UI] Greeting Page - Mobile View

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useUser } from "@auth0/nextjs-auth0/client"
 import { useRouter } from "next/router"
 import Home from "./screens/Home/Home"
+import Image from "next/image"
 
 function Index() {
   const router = useRouter()
@@ -19,13 +20,34 @@ function Index() {
   }
 
   return (
-    <div>
-      <button
-        className="px-10 py-2 rounded-full font-semibold hover:text-main"
-        onClick={auth0Login}
-      >
-        Login
-      </button>
+    <div className="bg-senthrap-splash bg-cover bg-center h-screen w-screen ">
+      <div className="flex flex-col items-center h-full text-center">
+        <div className="mx-12 mt-12">
+          <p className="text-4xl font-bold text-senthrap-yellow-100 drop-shadow-md		">
+            WELCOME TO SENTHRAP
+          </p>
+          <p className="text-senthrap-yellow-100 text-lg italic">
+            a mental health app
+          </p>
+        </div>
+        <div className=" items-center justify-center my-auto">
+          <Image
+            src="/assets/svg's/heart-senthrap.svg"
+            alt="Heart Senthrap"
+            width={160}
+            height={80}
+            className="mb-[70px]"
+          />
+        </div>
+        <button
+          className="bg-senthrap-neutral-100 rounded-md py-1 w-2/3 mb-12"
+          onClick={auth0Login}
+        >
+          <p className="text-senthrap-blue-100 text-xl font-bold">
+            Get Started
+          </p>
+        </button>
+      </div>
     </div>
   )
 }
