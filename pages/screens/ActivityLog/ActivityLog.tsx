a90f4a9a75bd6e0f0c6d356e7506e32e34b833d6
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-02-04 14:31:31 +0800

    [UI] New Act Log button + mood log ui adjustment

---
+++ pages/screens/ActivityLog/ActivityLog.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../Components/Header"
+
+function ActivityLog() {
+  return (
+    <div className="flex flex-col h-screen w-screen bg-senthrap-blue-100">
+      <Header />
+    </div>
+  )
+}
+
+export default ActivityLog
