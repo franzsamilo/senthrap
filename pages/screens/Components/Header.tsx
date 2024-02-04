215b486dc94eaf9d49c0521f9a3f98a615a63fc8
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-02-04 16:00:14 +0800

    [UI] Loading State for login + some adjusemnts

--- pages/screens/Components/Header.tsx
+++ pages/screens/Components/Header.tsx
@@ -6,22 +6,29 @@ function Header() {
   const { user } = useUser()
 
   return (
-    <div className="flex flex-row mx-8 mt-8 justify-between">
-      <Image
-        src={user?.picture || ""}
-        alt={user?.name || ""}
-        width={54}
-        height={48}
-        className="rounded-lg"
-      />
-      <button>
+    <div className="flex flex-row justify-between border-b-2 bg-senthrap-blue-100 border-senthrap-blue-200 shadow-md">
+      <div className="my-4 ml-4 flex flex-row items-center">
         <Image
-          src="/assets/svg's/nav-menu-button.svg"
-          alt="Mood Icon"
-          width={52}
-          height={47}
+          src={user?.picture || ""}
+          alt={user?.name || ""}
+          width={54}
+          height={48}
+          className="rounded-lg"
         />
-      </button>
+        <p className="text-senthrap-neutral-100 text-2xl font-bold ml-5 italic">
+          Hello, {user?.name ? user.name.split(" ")[0] : "friend"}!
+        </p>
+      </div>
+      <div className="my-4 mr-4">
+        <button>
+          <Image
+            src="/assets/svg's/nav-menu-button.svg"
+            alt="Mood Icon"
+            width={52}
+            height={47}
+          />
+        </button>
+      </div>
     </div>
   )
 }
