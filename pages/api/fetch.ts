a0c3734901e49e715ac49c22750c5b3173af9e39
cezro <hyderz1218@gmail.com>
2024-02-04 14:29:42 +0800

    added delete util

--- pages/api/fetch.ts
+++ pages/api/fetch.ts
@@ -1,6 +1,4 @@
 import type { NextApiRequest, NextApiResponse } from "next"
-import { db } from "./src/config/firebase"
-import { DocumentData, collection, getDocs } from "firebase/firestore"
 import getCollection from "./src/utils/getCollection"
 
 export default async function handler(
---
+++ pages/api/src/utils/deleteDocument.ts
@@ -0,0 +1,12 @@
+import { deleteDoc, doc } from "firebase/firestore"
+import { db } from "../config/firebase"
+
+async function deleteDocument(collectionName: string, documentId: string) {
+  try {
+    const documentRef = doc(db, collectionName, documentId)
+    await deleteDoc(documentRef)
+    console.log("Document successfully deleted!")
+  } catch (error) {
+    console.error("Error deleting document: ", error)
+  }
+}
