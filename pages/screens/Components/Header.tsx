199f1d33eeb67b3ffff343ecb4c2968d4d63ee0b
franzsamilo <franzeliezer.samilo-22@cpu.edu.ph>
2024-02-12 19:14:59 +0800

    ui : reworked home

--- pages/screens/Components/Header.tsx
+++ pages/screens/Components/Header.tsx
@@ -1,19 +1,28 @@
-import React from "react"
+import React, { useState } from "react"
 import Image from "next/image"
 import { useUser } from "@auth0/nextjs-auth0/client"
-import { IoHomeOutline } from "react-icons/io5"
-import useNavigation from "@/pages/api/src/Hooks/Navigation"
 import { useRouter } from "next/router"
+import useNavigation from "@/pages/api/src/Hooks/Navigation"
+import { IoHomeOutline, IoAperture, IoCloseOutline } from "react-icons/io5"
+import RadialMenu from "./RadialMenu"
 
 function Header() {
   const { user } = useUser()
   const { navigateToHome } = useNavigation()
   const router = useRouter()
+  const [isRadialMenuOpen, setIsRadialMenuOpen] = useState(false)
 
   const isHomeScreen = router.pathname === "/screens/Home/Home"
 
+  function toggleRadialMenu() {
+    setIsRadialMenuOpen(!isRadialMenuOpen)
+  }
+
   return (
     <div className="flex flex-row justify-between border-b-2 bg-senthrap-blue-100 border-senthrap-blue-200 shadow-md">
+      {isRadialMenuOpen && (
+        <div className="fixed inset-0 bg-black opacity-50 z-10" />
+      )}
       <div className="my-4 ml-4 flex flex-row items-center">
         <Image
           src={user?.picture || ""}
@@ -34,6 +43,31 @@ function Header() {
           >
             <IoHomeOutline className="w-10 h-10 " />
           </button>
+        </div>
+      )}
+
+      {isHomeScreen && (
+        <div className="mr-6 mt-6">
+          <button
+            onClick={toggleRadialMenu}
+            className="drop-shadow-lg text-white hover:text-senthrap-blue-50 rounded-full flex justify-center items-center"
+          >
+            <IoAperture className="w-10 h-10 " />
+          </button>
+        </div>
+      )}
+
+      {isRadialMenuOpen && (
+        <div className="fixed inset-0 flex justify-center items-center z-50">
+          <button
+            onClick={(e) => {
+              e.stopPropagation()
+              toggleRadialMenu()
+            }}
+          >
+            <IoCloseOutline className="text-red-500 w-16 h-16 absolute top-0 right-0 mt-5 mr-5" />
+          </button>
+          <RadialMenu />
         </div>
       )}
     </div>
--- pages/screens/Home/Home.tsx
+++ pages/screens/Home/Home.tsx
@@ -1,18 +1,67 @@
 import React from "react"
-import { useUser } from "@auth0/nextjs-auth0/client"
 import Header from "../Components/Header"
-import RadialMenu from "../Components/RadialMenu"
+import useNavigation from "@/pages/api/src/Hooks/Navigation"
+import { useRouter } from "next/router"
+import Image from "next/image"
 
 function Home() {
-  const { user } = useUser()
+  const {
+    navigateToActivityLog,
+    navigateToChat,
+    navigateToMoodLog,
+    navigateToStats,
+  } = useNavigation()
+
+  const router = useRouter()
+
+  function auth0Logout() {
+    router.push("/api/auth/logout")
+  }
 
   return (
-    <div className="flex flex-col min-h-screen w-screen  bg-senthrap-blue-100">
-      <div className=" font-bold items-center">
+    <div className="flex flex-col min-h-screen w-screen bg-senthrap-blue-100">
+      <div className="font-bold items-center">
         <Header />
-        <p className="m-4 text-white">{user?.email}</p>
-        <div className="mt-24">
-          <RadialMenu />
+        <div className="border-t-4 border-senthrap-blue-50 drop-shadow-lg flex flex-col">
+          <div className="items-center flex flex-col">
+            <Image
+              src="/assets/senthrap-logo-no-bg.png"
+              alt="Heart Senthrap"
+              width={200}
+              height={100}
+            />
+          </div>
+          <div className="border-t-4 border-senthrap-blue-50 drop-shadow-lg mb-24" />
+          <button
+            onClick={navigateToChat}
+            className="mt-4 py-2 px-auto mx-12 bg-white rounded-2xl text-2xl text-senthrap-blue-50 hover:bg-senthrap-blue-200 hover:text-white "
+          >
+            Go to Chat
+          </button>
+          <button
+            onClick={navigateToMoodLog}
+            className="mt-4 py-2 px-auto mx-12 bg-white rounded-2xl text-2xl text-senthrap-blue-50 hover:bg-senthrap-blue-200 hover:text-white"
+          >
+            Go to Mood Log
+          </button>
+          <button
+            onClick={navigateToActivityLog}
+            className="mt-4 py-2 px-auto mx-12 bg-white rounded-2xl text-2xl text-senthrap-blue-50 hover:bg-senthrap-blue-200 hover:text-white"
+          >
+            Go to Activity Log
+          </button>
+          <button
+            onClick={navigateToStats}
+            className="mt-4 py-2 px-auto mx-12 bg-white rounded-2xl text-2xl text-senthrap-blue-50 hover:bg-senthrap-blue-200 hover:text-white"
+          >
+            Go to Stats
+          </button>
+          <button
+            className="mt-4 py-2 px-auto mx-12 bg-red-500 rounded-2xl text-2xl text-white hover:bg-red-800"
+            onClick={auth0Logout}
+          >
+            Log Out
+          </button>
         </div>
       </div>
     </div>
