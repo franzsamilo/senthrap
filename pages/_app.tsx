101cc55d470eee2257f039f5cc59301e636126ca
jedmamosto <jedmamosto@gmail.com>
2024-04-04 14:06:45 +0800

    config: added Montserrat font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import "@/styles/globals.css"
 import type { AppProps } from "next/app"
 import { UserProvider } from "@auth0/nextjs-auth0/client"
+import { Montserrat } from "next/font/google"
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  fallback: ["Arial", "sans-serif"],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <main className={montserrat.className}>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </main>
   )
 }
